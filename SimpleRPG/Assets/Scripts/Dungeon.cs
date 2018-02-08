544f7928b61805f5611b757d0547e53803f92738
Alex Lysenko <alicewithalex25@gmail.com>
2018-02-08 11:05:09 +0300

    Add Dungeon Generator
    Still need Fix some moments

---
+++ SimpleRPG/Assets/Scripts/Dungeon.cs
@@ -0,0 +1,586 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour {
+
+
+	public GameObject Floor;
+	public GameObject Wall;
+	[SerializeField]
+	public const int X = 100;
+	public const int Y = 100;
+	private int NumberOfRooms=13;
+	int RealNumberOfRooms;
+
+	char [,] grid=new char[Y,X];
+
+	Leaf [] leafs;
+
+	int RBH,RBW,RH,RW,_difference,NX,NY;
+
+	char freeSpace='.';
+	char border='#';
+	char coridor='o';
+
+
+
+	// Use this for initialization
+	void Start () {
+		if (X * Y / NumberOfRooms > 20) {
+			GenerateMap();
+			GenerateLeafs();
+			Generate3D();
+		}
+	}
+
+	public void Generate3D(){
+		GameObject Map = new GameObject ("Dungeon");
+		Map.transform.position = new Vector3 (0, 0, 0);
+		for (int i = 0; i < Y; i++) {
+			for (int j = 0; j < X; j++) {
+				if (grid [i, j] == freeSpace) {
+					
+					Instantiate (Wall, new Vector3 (j * Wall.transform.lossyScale.z, 0+Wall.transform.lossyScale.y/2, i * Wall.transform.lossyScale.x), Quaternion.identity,Map.transform);
+
+				} else if (grid [i, j] == border || grid [i, j] == coridor) {
+					
+					Instantiate (Floor, new Vector3 (j * Floor.transform.lossyScale.z,0.5f, i * Floor.transform.lossyScale.x), Quaternion.identity,Map.transform);
+					Floor.name = "Row" + j + " Column" + i;
+
+
+				}
+			}
+			
+		}
+	}
+	
+	public void GenerateMap(){
+		
+	for (int i = 0; i < Y; i++) {
+		for (int j = 0; j < X; j++) {
+			grid [i,j] = freeSpace;
+			}
+		}
+
+	}
+
+	public int GetAmountOfRooms(){
+		RBH=(int)Mathf.Sqrt(NumberOfRooms);
+		RBW=(int)NumberOfRooms/RBH;
+
+		_difference=NumberOfRooms-RBH*RBW;
+
+		if(_difference==0)
+		{
+			RealNumberOfRooms=NumberOfRooms;
+		}
+		else
+		{
+			if(RBH>RBW)
+			{
+				RBW++;
+			}
+			else if(RBW>RBH)
+			{
+				RBH++;
+			}
+			else
+			{
+				if(Random.Range(0.0f,1.0f)>0.5f)
+				{
+					RBW++;
+				}
+				else RBH++;
+			}
+
+		}
+		RealNumberOfRooms=RBH*RBW;
+
+		int temp;
+
+		if(X>Y)
+		{
+			if(RBW<RBH)
+			{
+				temp=RBH;
+				RBH=RBW;
+				RBW=temp;
+				temp=0;
+			}
+		}
+		else if( X < Y )
+		{
+			if(RBW > RBH)
+			{
+				temp=RBW;
+				RBW=RBH;
+				RBH=temp;
+				temp=0;
+			}
+		}
+		else
+		{
+			if(Random.Range(0.0f,1.0f)>0.5f)
+			{
+				temp=RBW;
+				RBW=RBH;
+				RBH=temp;
+				temp=0;
+			}
+			else
+			{
+				temp=RBH;
+				RBH=RBW;
+				RBW=temp;
+				temp=0;
+			}
+		}
+
+		RH=Y/RBH;
+		RW=X/RBW;
+
+		return RealNumberOfRooms;
+	}
+
+	public void GenerateLeafs(){
+		
+		int amount=GetAmountOfRooms();
+
+		leafs = new Leaf[amount];
+
+		for (int i = 0; i < amount; i++) {
+			leafs[i] = new Leaf();
+		}
+
+		int MinX=(int)RW/2;
+		int MinY=(int)RH/2;
+
+		for(int i=0;i<amount;i++)
+		{
+			leafs[i].merged =false;
+			leafs[i].connected=false;
+			leafs[i].ID=-1;
+		}
+
+		for(int A=0;A<amount;A++)
+		{
+			NX=(int)A%RBW;
+			NY=(int)A/RBW;
+
+			//Локальные координаты
+			leafs[A].x=Random.Range(0,(RW-MinX));
+			leafs[A].y=Random.Range(0,(RH-MinY));
+			// Генерируем высоту и ширину в зависимости от локальных координат
+			leafs[A].w=Random.Range(MinX,(RW-leafs[A].x));
+			leafs[A].h=Random.Range(MinY,(RH-leafs[A].y));
+			// Переводим локальные координаты в глобальные
+			leafs[A].x += (NX*RW+1);
+			leafs[A].y += (NY*RH+1);
+			Debug.Log ("X:" + leafs [A].x + " Y:" + leafs [A].y + " W:" + leafs [A].w + " H:" + leafs [A].h);
+
+
+			for(int i=leafs[A].y;i<leafs[A].y+leafs[A].h;i++)
+			{
+				for( int j=leafs[A].x;j<leafs[A].x+leafs[A].w;j++)
+				{
+					//	Debug.Log ("i:" + i + " j:" + j + "Number of Room:" + A);
+					grid[i,j]=border;
+				}
+			}
+		}
+		// Начинаем растягивание комнат
+		int idCounter=0;
+		int diff=0;
+		int breakCounter;
+		do{
+			for(int i=0;i<amount;i++)
+			{
+
+				if(Random.value>0.85f)
+				{
+
+
+					breakCounter=0;
+					int direction=Random.Range(1,5);
+					do{
+
+
+
+						if( leafs[i].ID==-1 && (diff!=amount-NumberOfRooms))
+						{
+
+							int tx1,tx2,ty1,ty2;
+							switch(direction)
+							{
+							case 1:    // вверх
+								{
+									if((int)i/RBW>0 && leafs[i-RBW].ID==-1)
+									{
+										ty1=leafs[i-RBW].y;
+										ty2=leafs[i].y+leafs[i].h;
+
+										if(leafs[i-RBW].x<leafs[i].x)
+										{
+											tx1=leafs[i-RBW].x;
+											if(leafs[i-RBW].x+leafs[i-RBW].w < leafs[i].x+leafs[i].w )
+											{
+												tx2=leafs[i].x+leafs[i].w;
+											}
+											else tx2=leafs[i-RBW].x+leafs[i-RBW].w;
+
+
+
+
+										}
+										else if(leafs[i-RBW].x>leafs[i].x)
+										{
+											tx1=leafs[i].x;
+											if(leafs[i-RBW].x+leafs[i-RBW].w < leafs[i].x+leafs[i].w )
+											{
+												tx2=leafs[i].x+leafs[i].w;
+											}
+											else tx2=leafs[i-RBW].x+leafs[i-RBW].w;
+
+
+										}
+										else
+										{
+											tx1=leafs[i].x;
+											if(leafs[i].w < leafs[i-RBW].w)
+											{
+												tx2=tx1+leafs[i-RBW].w;
+											}
+											else if(leafs[i].w > leafs[i-RBW].w)
+											{
+												tx2=tx1+leafs[i].w;
+											}
+											else tx2=tx1+leafs[i].w;
+										}
+
+										for(int q=ty1;q<ty2;q++)
+										{
+											for(int j=tx1;j<tx2;j++)
+											{
+												grid[q,j]=border;
+												//Draw();
+											}
+										}
+										leafs[i].x=tx1;
+										leafs[i-RBW].x=tx1;
+										leafs[i].w=tx2-tx1;;
+										leafs[i-RBW].w=tx2-tx1;;
+
+										leafs[i].y=ty1;
+										leafs[i-RBW].y=ty1;
+										leafs[i].h=ty2-ty1;
+										leafs[i-RBW].h=ty2-ty1;
+
+										leafs[i].ID=idCounter;
+										leafs[i-RBW].ID=idCounter;
+										diff++;
+									}
+									break;
+								}
+							case 2:   // влево
+								{
+									if((int)i%RBW>0 && leafs[i-1].ID==-1)
+									{
+										tx1=leafs[i-1].x;
+										tx2=leafs[i].x+leafs[i].w;
+
+										if(leafs[i-1].y < leafs[i].y)
+										{
+											ty1=leafs[i-1].y;
+											if(leafs[i-1].y+leafs[i-1].h < leafs[i].y+leafs[i].h)
+											{
+												ty2=leafs[i].y+leafs[i].h;
+											}
+											else ty2=leafs[i-1].y+leafs[i-1].h;
+										}
+										else if(leafs[i-1].y > leafs[i].y)
+										{
+											ty1=leafs[i].y;
+											if(leafs[i-1].y+leafs[i-1].h < leafs[i].y+leafs[i].h)
+											{
+												ty2=leafs[i].y+leafs[i].h;
+											}
+											else ty2=leafs[i-1].y+leafs[i-1].h;
+
+										}
+										else
+										{
+											ty1=leafs[i].y;
+											if(leafs[i].h > leafs[i-1].h)
+											{
+												ty2=ty1+leafs[i].h;
+											}
+											else if(leafs[i].h < leafs[i-1].h)
+											{
+												ty2=ty1+leafs[i-1].h;
+											}
+											else ty2=ty1+leafs[i].h;
+										}
+
+										for(int q=ty1;q<ty2;q++)
+										{
+											for(int j=tx1;j<tx2;j++)
+											{
+												grid[q,j]=border;
+												//Draw();
+											}
+										}
+
+										leafs[i].x=tx1;
+										leafs[i-1].x=tx1;
+										leafs[i].w=tx2-tx1;;
+										leafs[i-1].w=tx2-tx1;;
+
+										leafs[i].y=ty1;
+										leafs[i-1].y=ty1;
+										leafs[i].h=ty2-ty1;
+										leafs[i-1].h=ty2-ty1;
+
+										leafs[i].ID=idCounter;
+										leafs[i-1].ID=idCounter;
+										diff++;
+									}
+									break;
+								}
+							case 3: // вниз
+								{
+									if((int)i/RBW<RBH-1 && leafs[i+RBW].ID==-1)
+									{
+										ty1=leafs[i].y;
+										ty2=leafs[i+RBW].y+leafs[i+RBW].h;
+
+										if(leafs[i+RBW].x<leafs[i].x)
+										{
+											tx1=leafs[i+RBW].x;
+											if(leafs[i+RBW].x+leafs[i+RBW].w < leafs[i].x+leafs[i].w )
+											{
+												tx2=leafs[i].x+leafs[i].w;
+											}
+											else tx2=leafs[i+RBW].x+leafs[i+RBW].w;
+
+										}
+										else if(leafs[i+RBW].x>leafs[i].x)
+										{
+											tx1=leafs[i].x;
+											if(leafs[i+RBW].x+leafs[i+RBW].w < leafs[i].x+leafs[i].w )
+											{
+												tx2=leafs[i].x+leafs[i].w;
+											}
+											else tx2=leafs[i+RBW].x+leafs[i+RBW].w;
+
+										}
+										else
+										{
+											tx1=leafs[i].x;
+											if(leafs[i].w < leafs[i+RBW].w)
+											{
+												tx2=tx1+leafs[i+RBW].w;
+											}
+											else if(leafs[i].w >leafs[i+RBW].w)
+											{
+												tx2=tx1+leafs[i].w;
+											}
+											else tx2=tx1+leafs[i].w;
+										}
+
+										for(int q=ty1;q<ty2;q++)
+										{
+											for(int j=tx1;j<tx2;j++)
+											{
+												grid[q,j]=border;
+												//Draw();
+											}
+										}
+
+										leafs[i].x=tx1;
+										leafs[i+RBW].x=tx1;
+										leafs[i].w=tx2-tx1;;
+										leafs[i+RBW].w=tx2-tx1;;
+
+										leafs[i].y=ty1;
+										leafs[i+RBW].y=ty1;
+										leafs[i].h=ty2-ty1;
+										leafs[i+RBW].h=ty2-ty1;
+
+										leafs[i].ID=idCounter;
+										leafs[i+RBW].ID=idCounter;
+										diff++;
+									}
+									break;
+								}
+							case 4:    // вправо
+								{
+									if((int)i%RBW<RBW-1 && leafs[i+1].ID==-1)
+									{
+										tx1=leafs[i].x;
+										tx2=leafs[i+1].x+leafs[i+1].w;
+
+
+										if(leafs[i+1].y < leafs[i].y)
+										{
+											ty1=leafs[i+1].y;
+											if(leafs[i+1].y+leafs[i+1].h < leafs[i].y+leafs[i].h)
+											{
+												ty2=leafs[i].y+leafs[i].h;
+											}
+											else ty2=leafs[i+1].y+leafs[i+1].h;
+
+										}
+										else if(leafs[i+1].y > leafs[i].y)
+										{
+											ty1=leafs[i].y;
+											if(leafs[i+1].y+leafs[i+1].h < leafs[i].y+leafs[i].h)
+											{
+												ty2=leafs[i].y+leafs[i].h;
+											}
+											else ty2=leafs[i+1].y+leafs[i+1].h;
+
+										}
+										else
+										{
+											ty1=leafs[i].y;
+											if(leafs[i].h >leafs[i+1].h)
+											{
+												ty2=ty1+leafs[i].h;
+											}
+											else if(leafs[i].h < leafs[i+1].h)
+											{
+												ty2=ty1+leafs[i+1].h;
+											}
+											else ty2=ty1+leafs[i].h;
+										}
+
+										for(int q=ty1;q<ty2;q++)
+										{
+											for(int j=tx1;j<tx2;j++)
+											{
+												grid[q,j]=border;
+												//Draw();
+											}
+										}
+
+										leafs[i].x=tx1;
+										leafs[i+1].x=tx1;
+										leafs[i].w=tx2-tx1;
+										leafs[i+1].w=tx2-tx1;
+
+										leafs[i].y=ty1;
+										leafs[i+1].y=ty1;
+										leafs[i].h=ty2-ty1;
+										leafs[i+1].h=ty2-ty1;
+
+										leafs[i].ID=idCounter;
+										leafs[i+1].ID=idCounter;
+										diff++;
+									}
+									break;
+								}
+							}
+							idCounter++;
+							direction++;
+							if(direction==5)
+								direction=1;
+							breakCounter++;
+							if(breakCounter==4)
+								break;
+
+						}else if(diff==amount-NumberOfRooms)
+						{
+							break;
+						}
+					}while(leafs[i].ID==-1);
+				}
+			}
+		}while(diff!=amount-NumberOfRooms);
+
+		// Коридоры
+
+		for(int a=0;a<amount;a++)
+		{
+			int cx1,cx2,cy1,cy2;
+			int direction=Random.Range(1,5);
+			int counter=0;
+			do
+			{
+				if(!leafs[a].connected)
+					switch(direction)
+				{
+				case 1:
+					{
+						if((int)a/RBW>0 && (leafs[a-RBW].ID==-1 || leafs[a-RBW].ID!=leafs[a].ID) && !leafs[a-RBW].connected)
+						{
+							cx1=MinX+a%RBW*RW;
+							cy1=leafs[a-RBW].y+leafs[a-RBW].h;
+							cy2=leafs[a].y;
+							for(int i=cy1;i<cy2;i++)
+							{
+								grid[i,cx1]=coridor;
+							}
+							leafs[a].connected=true;
+						}
+						break;
+					}
+				case 2:
+					{
+						if((int)a%RBW>0 && (leafs[a-1].ID==-1 || leafs[a-1].ID!=leafs[a].ID) && !leafs[a-1].connected)
+						{
+							cy1=MinY+a/RBW*RH;
+							cx1=leafs[a-1].x+leafs[a-1].w;
+							cx2=leafs[a].x;
+							for(int i=cx1;i<cx2;i++)
+							{
+								grid[cy1,i]=coridor;
+							}
+							leafs[a].connected=true;
+						}
+						break;
+					}
+				case 3:
+					{
+						if((int)a/RBW<RBH-1 && (leafs[a+RBW].ID==-1 || leafs[a+RBW].ID!=leafs[a].ID) && !leafs[a+RBW].connected)
+						{
+							cx1=MinX+a%RBW*RW;
+							cy1=leafs[a].y+leafs[a].h;
+							cy2=leafs[a+RBW].y;
+							for(int i=cy1;i<cy2;i++)
+							{
+								grid[i,cx1]=coridor;
+							}
+							leafs[a].connected=true;
+						}
+						break;
+					}
+				case 4:
+					{
+						if((int)a%RBW<RBW-1 && (leafs[a+1].ID==-1 || leafs[a+1].ID!=leafs[a].ID) && !leafs[a+1].connected)
+						{
+							cy1=MinY+a/RBW*RH;
+							cx1=leafs[a].x+leafs[a].w;
+							cx2=leafs[a+1].x;
+							for(int i=cx1;i<cx2;i++)
+							{
+								grid[cy1,i]=coridor;
+							}
+							leafs[a].connected=true;
+						}
+						break;
+					}
+				}
+				direction++;
+				if(direction==5)
+					direction=1;
+
+				counter++;
+				if(counter==4)
+					break;
+
+			}while(!leafs[a].connected);
+		}
+	}
+		
+}
--- SimpleRPG/Assets/Scripts/Leaf.cs
+++ SimpleRPG/Assets/Scripts/Leaf.cs
@@ -1,78 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public struct intVector2{
+public class Leaf 
+{
 	public int x;
 	public int y;
+	public int w;
+	public int h;
+	public bool connected;
+	public bool merged;
+	public int ID;
 
-	public intVector2(int a,int b){
-		x = a;
-		y = b;
+	public Leaf(){
+		x = 0;
+		y = 0;
+		w = 0;
+		h = 0;
+		connected = false;
+		merged = false;
+		ID = -1;
 	}
 }
 
-public class Leaf {
-
-	private const int MIN_LEAF_SIZE=6;
-
-	public int x, y, width, height;
-
-	public Leaf leftChild;
-	public Leaf rightChild;
-	public Room room;
-	public List<Room> halls;
-
-	public Leaf(int X,int  Y,int  Width,int  Height){
-		x = X;
-		y = Y;
-		width = Width;
-		height = Height;
-
-	}
-
-	public bool split(){
-		if (leftChild != null && rightChild != null)
-			return false;
-
-		bool splitH = Random.value > 0.5f;
-		if (width > height && width / height > 1.25f)
-			splitH = false;
-		else if (height > width && height / width > 1.25f)
-			splitH = true;
-
-		int max = (splitH ? height : width) - MIN_LEAF_SIZE;
-
-		if (max <= MIN_LEAF_SIZE)
-			return false;
-		
-		int split = Random.Range (MIN_LEAF_SIZE, max + 1);
-
-		if (splitH) {
-			leftChild = new Leaf (x, y, width, split);
-			rightChild = new Leaf (x, y + split, width, height - split);
-		} else {
-			leftChild = new Leaf (x, y, split, height);
-			rightChild = new Leaf (x + split, y, width - split, height);
-		}
-
-		return true;
-	}
-
-	public void createRooms(){
-		if (leftChild != null || rightChild != null) {
-			if (leftChild != null) {
-				leftChild.createRooms ();
-			}
-			if (rightChild != null) {
-				rightChild.createRooms ();
-			}
-		} else {
-			intVector2 roomSize;
-			intVector2 roomPos;
-			roomSize = new intVector2 (Random.Range (3, width - 1), Random.Range (3, height - 1));
-			roomPos = new intVector2 (Random.Range (1, width - roomSize.x), Random.Range (1, height - roomSize.y));
-			room = new Room (x + roomPos.x, y + roomPos.y, roomSize.x, roomSize.y);
-		}
-	}
-}
